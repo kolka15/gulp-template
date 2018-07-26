004a935343eda4477f491d6c8e73b0b40d948cbc
kozhushko <d.kozhushko@informika.ru>
2018-07-26 13:19:25 +0300

    gulp settings in progress

---
+++ gulpfile.js
@@ -0,0 +1,99 @@
+const gulp = import('gulp');
+
+const sass = require('gulp-sass');
+const sassGlob = require('gulp-sass-glob');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+
+const webpack = require('webpack-stream');
+
+const browserSync = import('browser-sync').create();
+const reload = browserSync.reload;
+
+const path = {
+    src: './src',
+    srcCss: `${this.src}/css`,
+    srcJS: `${this.src}/js`,
+    srcAssets: `${this.src}/assets`,
+    dist: './dist',
+    distCss: `${this.dist}/css`,
+    distJs: `${this.dist}/js`,
+};
+
+const browsrSyncOpts = {
+    https: false,
+    open: false,
+    notify: false,
+    ghostMode: false,
+    server: {
+        baseDir: "./src"
+    }
+};
+
+const cleanCssOpts = {
+    compatibility: 'ie9',
+    level: 2
+};
+
+
+/**
+ *
+ *  sass
+ *
+ * */
+
+gulp.task('sass', function () {
+    return gulp.src(`${path.srcCss}/main.scss`)
+        .pipe(sassGlob())
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        // .on('error', swallowError)
+        .pipe(sourcemaps.write('.'))
+        // .on('error', swallowError)
+        .pipe(gulp.dest(`${path.srcAssets}/css`))
+        .pipe(browserSync.stream())
+});
+
+gulp.task('sass:build', function () {
+    return gulp.src(`${path.srcCss}/main.scss`)
+        .pipe(sassGlob())
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 3 versions', 'ie >=9']
+        }))
+        .pipe(cleanCSS(cleanCssOpts))
+        .pipe(gulp.dest(path.distCss))
+});
+
+
+/**
+ *
+ *  js
+ *
+ * */
+
+gulp.task('js:build', function() {
+    return gulp.src('src/entry.js')
+        .pipe(webpack())
+        .pipe(gulp.dest('dist/'));
+});
+
+
+/**
+ *
+ *  watcher
+ *
+ * */
+
+gulp.task('go', function () {
+    browserSync.init(browsrSyncOpts);
+    gulp.watch(`${path.srcCss}/**/*.scss`, gulp.series('sass'));
+    gulp.watch(`${path.srcJS}/**/*.js`, gulp.series('js')).on('change', reload);
+    gulp.watch(`${path.src}/*.html`).on('change', reload);
+});
+
+function swallowError(error) {
+    console.log(error.toString());
+    this.emit('end')
+}
